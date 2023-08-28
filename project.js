715dab38030a035d06de5f01fa6996b0b9eda0fb
Ajfar001 <91411405+Ajfar001@users.noreply.github.com>
2023-08-28 19:12:32 +0530

    Add files via upload

---
+++ project.js
@@ -0,0 +1,8 @@
+
+var typed=new Typed(".text",{
+ strings: ["Frontend Developer", "Backend Developer", "Full Stack Developer"],
+    typeSpeed: 100,
+    BackSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
